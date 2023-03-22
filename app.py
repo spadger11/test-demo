5c230eb7dbfe19f54698ff671cc344575b42642c
xingyu <xkts_mky@163.com>
2023-03-22 10:12:31 +0800

    修改文件

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,  request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 app = Flask(__name__)
 # MySQl所在的主机名
 HOSTNAME = "127.0.0.1"
